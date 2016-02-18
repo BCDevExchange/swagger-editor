1d11f2866ba49c77ed0a0d3b396d50f10ffb1e9e
f-w <fred.wen@gov.bc.ca>
2016-02-18 13:48:08 -0800

    added jit-grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@
 module.exports = function (grunt) {
 
   require('load-grunt-config')(grunt);
+  require('jit-grunt')(grunt);
   require('time-grunt')(grunt);
 
   grunt.registerTask('serve', function (target) {
